ac7e3b5bcce063e4031ceba6d8629b9c2101f0ed
peng.chen <peng.chen@weimob.com>
2019-01-30 10:46:20 +0800

    scroll-words

---
+++ src/components/scrollWord.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="swiper-container-scroll">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide swiper-no-swiping" :key="i" v-for="(item, i) in data">{{item}}</div>
+    </div>
+  </div>
+</template>
+<script>
+import Swiper from "swiper";
+import "../assets/css/swiper.min.css";
+export default {
+  name: "swiper-component",
+  data () {
+    return {
+      swiper: null
+    };
+  },
+  props: ["data"],
+  mounted () {
+    this.swiper = new Swiper(".swiper-container-scroll", {
+      direction: "vertical",
+      loop: true,
+      noSwiping: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false
+      }
+    });
+  }
+};
+</script>
+<style lang="less">
+.swiper-container-scroll {
+  width: 400px;
+  height: 30px;
+  overflow: hidden;
+  line-height: 30px;
+  border: 1px solid red;
+  .swiper-wrapper {
+    .swiper-slide {
+      //width:100%;
+      &:nth-child(1) {
+        background-color: aqua;
+      }
+      &:nth-child(2) {
+        background-color: red;
+      }
+      &:nth-child(3) {
+        background-color: blue;
+      }
+      &:nth-child(4) {
+        background-color: green;
+      }
+    }
+  }
+}
+</style>
--- src/pages/item/index.vue
+++ src/pages/item/index.vue
@@ -4,17 +4,25 @@
             item-title
         </div>
         <test-swiper></test-swiper>
+        <scroll-word :data="scrollWords"></scroll-word>
     </div>
 </template>
 <script>
 import testSwiper from '@/components/swiper.vue';
+import testScrollSwiper from '@/components/scrollWord.vue';
 export default {
     name: 'item-page',
+    data () {
+        return {
+            scrollWords:['1111111', '22222222', '3333333', '4444444']
+        }
+    },
     mounted () {
         console.log(this.$route);
     },
     components:{
-        testSwiper:testSwiper
+        testSwiper:testSwiper,
+        scrollWord:testScrollSwiper
     }
 }
 </script>
@@ -23,6 +31,6 @@ export default {
     .item{
         .title{
             background-color:orange
-        }   
+        }
     }
 </style>
